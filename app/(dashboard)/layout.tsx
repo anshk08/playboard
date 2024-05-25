50adc80c603b593027343caad7045ef00e605577
Divyansh <anshkanojia2@gmail.com>
2024-05-25 23:35:16 +0530

    updated dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,17 +1,17 @@
-import Navbar from "./_components/navbar";
-import OrgSidebar from "./_components/org-sidebar";
-import { Sidebar } from "./_components/sidebar";
+import { Sidebar } from "@/app/(dashboard)/_components/sidebar";
+import OrgSidebar from "@/app/(dashboard)/_components/org-sidebar";
+import Navbar from "@/app/(dashboard)/_components/navbar";
 
-interface DashboardLayoutProps {
+type DashboardLayoutProps = {
   children: React.ReactNode;
-}
+};
 
-export const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
-    <main className="h-full">
+    <main className={"h-full"}>
       <Sidebar />
-      <div className="pl-[60px] h-full">
-        <div className="flex gap-x-3 h-full">
+      <div className="h-full pl-[60px]">
+        <div className={"flex h-full gap-x-3"}>
           <OrgSidebar />
           <div className="h-full flex-1">
             <Navbar />
@@ -21,6 +21,4 @@ export const DashboardLayout = ({ children }: DashboardLayoutProps) => {
       </div>
     </main>
   );
-};
-
-export default DashboardLayout;
+}
